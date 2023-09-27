430cb9f641a28ace42c1ebae5d6f21628a21e31a
ZajcevDima <zaycevdi@gmail.com>
2023-09-27 13:39:15 +0300

    feat: KS-1-2 добавлена новая переменная

---
+++ src/main/kotlin/lesson_1/lesson_1_task_2.kt
@@ -0,0 +1,15 @@
+package lesson_1
+
+fun main() {
+    val numberOfOrders: Int = 75
+    val textThanking: String = "Спасибо за покупку!"
+    var amountOfWorkers: Int = 2000
+
+    println(numberOfOrders)
+    println(textThanking)
+
+//    println(amountOfWorkers)
+
+    amountOfWorkers = 1999
+    println(amountOfWorkers)
+}
